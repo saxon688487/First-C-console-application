c8f1bd8657de756cb82a2be9ca7173ae4d1dfb05
Saxon Lea <saxon688487@gmail.com>
2017-08-06 15:45:21 -0700

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        SimpleClass.printBaseline();
+
+
+        Console.WriteLine("Press ENTER to continue...");
+        Console.ReadLine();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+            Console.Beep();
+        }
+    }
+
+    static string promptInput()
+    {
+        System.Console.WriteLine("Not in OtherNameSpace.Program right now.");
+        return "";
+    }
+}
+
+class SimpleClass
+{
+    static readonly DateTime baseline;
+
+    static SimpleClass()
+    {
+        baseline = DateTime.Today;
+        Console.WriteLine("SimpleClass static constructor");
+    }
+
+    public static void printBaseline()
+    {
+        Console.WriteLine(baseline);
+    }
+}
